5c3a2d1b9df3fb18104be90b38f7d93adb5c9b27
Matthew Martin <matthew@hellois0.com>
2024-07-22 08:55:20 -0500

    Resolved 'UnidentifiedImageError(msg)' by adding an if statement to ensure the file isn't a .DS_STORE file before attempting conversions

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ output_directory = "/opt/icons"
 
 # For loop to make updates and save images
 for file in os.listdir(directory):
-    im = Image.open(os.path.join(directory, file)) # Open image
-    im = im.rotate(-90) # Rotate image 90 degrees
-    im = im.resize((128, 128)) # Resize image to 128x128
-    im = im.convert("RGB") # Convert files to RGB
-    im.save(os.path.join(output_directory, file+".jpeg")) # Save the image to new folder as .jpeg
+    if file != ".DS_Store": # Check filename to ensure image file
+        im = Image.open(os.path.join(directory, file)) # Open image
+        im = im.rotate(-90) # Rotate image 90 degrees
+        im = im.resize((128, 128)) # Resize image to 128x128
+        im = im.convert("RGB") # Convert files to RGB
+        im.save(os.path.join(output_directory, file+".jpeg")) # Save the image to new folder as .jpeg
