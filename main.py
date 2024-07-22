e7a93dfadfa296e87b5d3dfb940af77b3af7774e
Matthew Martin <matthew@hellois0.com>
2024-07-22 08:51:27 -0500

    Resolved 'OSError: cannot write mode LA as JPEG' by using module conversion function to convert files to RGB

--- main.py
+++ main.py
@@ -12,4 +12,5 @@ for file in os.listdir(directory):
     im = Image.open(os.path.join(directory, file)) # Open image
     im = im.rotate(-90) # Rotate image 90 degrees
     im = im.resize((128, 128)) # Resize image to 128x128
+    im = im.convert("RGB") # Convert files to RGB
     im.save(os.path.join(output_directory, file+".jpeg")) # Save the image to new folder as .jpeg
